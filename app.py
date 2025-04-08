459f7df8f16d82111bfbb338ab90f9b51c06751a
Will Xu <xwywryx@outlook.com>
2025-04-08 23:57:12 +0800

    :sparkles: Add new index made like juejin.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index_juejin.html")
 
 @app.route("/posts")
 def get_posts():
