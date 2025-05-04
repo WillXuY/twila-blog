40972d442be03aae2e4d568077a940fc59b47d39
Will Xu <xwywryx@outlook.com>
2025-05-04 23:31:47 +0800

    :sparkles: PostgreSQL 在 podman 中运行并初始化项目所需的数据库。

--- scripts/postgresql/init_database.sql
+++ scripts/postgresql/init_database.sql
@@ -1,35 +1,40 @@
--- 1. 创建数据库
-CREATE DATABASE htmx_flask_blog_data;
+-- I. 创建数据库与用户 (!!! 需要修改密码 !!!)
+-- 1. 创建 DBA 用户（拥有数据库）
+-- 2. 创建程序用户
+-- 3. 创建数据库并设置 owner 为 DBA
 
--- 2. 创建用户并设置密码
-CREATE USER htmx_flask_blog_user WITH PASSWORD 'your_password';
+CREATE USER twila_admin WITH
+  LOGIN
+  ENCRYPTED PASSWORD 'password.';
 
--- 3. 赋予用户对数据库的所有权限
-GRANT ALL PRIVILEGES ON DATABASE htmx_flask_blog_data TO htmx_flask_blog_user;
+CREATE USER twila_app WITH
+  LOGIN
+  ENCRYPTED PASSWORD 'pw.';
 
--- 4. 切换到新创建的数据库
-\c htmx_flask_blog_data
+CREATE DATABASE twila_blog
+  OWNER twila_admin
+  ENCODING 'UTF8'
+  LC_COLLATE = 'en_US.UTF-8'
+  LC_CTYPE = 'en_US.UTF-8'
+  TEMPLATE template0;
 
--- 5. 创建 posts 表格
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL
-);
+-- II. 授权
+-- 1. 授予程序用户访问数据库权限
+-- 2. 授予 public schema 使用权限
+-- 3. 授权现有对象（仅限已有的）
+-- 4. 设置未来默认权限（由 twila_admin 创建的表/序列，自动授权给 twila_app）
 
--- 6. 为所有现有表格授予权限
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO htmx_flask_blog_user;
+\connect twila_blog
 
--- 7. 为所有现有序列授予权限
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO htmx_flask_blog_user;
+GRANT CONNECT ON DATABASE twila_blog TO twila_app;
 
--- 8. 确保对将来创建的表和序列也有权限
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT ALL PRIVILEGES ON TABLES TO htmx_flask_blog_user;
+GRANT USAGE ON SCHEMA public TO twila_app;
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT ALL PRIVILEGES ON SEQUENCES TO htmx_flask_blog_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO twila_app;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO twila_app;
 
--- 9. 确保对将来创建的函数也有权限
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT EXECUTE ON FUNCTIONS TO htmx_flask_blog_user;
+ALTER DEFAULT PRIVILEGES FOR USER twila_admin IN SCHEMA public
+  GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO twila_app;
+
+ALTER DEFAULT PRIVILEGES FOR USER twila_admin IN SCHEMA public
+  GRANT USAGE, SELECT ON SEQUENCES TO twila_app;
